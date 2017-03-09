bcefc1ae9098fcfee82b896bef83a6330f14dff2
alex <alex@alexdeMacBook-Pro.local>
2017-03-09 08:51:16 +0800

    template

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var router = require('koa-router')();
+
+router.get('/', async function (ctx, next) {
+  ctx.state = {
+    title: 'koa2 title'
+  };
+
+  await ctx.render('index', {
+  });
+})
+router.get('/create', async function (ctx, next) {
+  ctx.state = {
+    title: 'koa2 title'
+  };
+
+  await ctx.render('index', {
+  });
+})
+module.exports = router;
